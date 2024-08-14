b2a3fbad3c387b402b468c1d2c98fd8548750046
jackal7819 <jackal3166@gmail.com>
2024-08-14 17:54:41 +0300

    Add ErrorPage

--- app/error.tsx
+++ app/error.tsx
@@ -1,18 +1,21 @@
-import Link from 'next/link';
-import { FaExclamationTriangle } from 'react-icons/fa';
+'use client';
 
-export default function NonFoundPage() {
+import Error from 'next/error';
+import Link from 'next/link';
+import { FaExclamationCircle } from 'react-icons/fa';
+
+export default function ErrorPage({ error }: { error: Error }) {
 	return (
 		<section className='flex items-center justify-center w-full bg-blue-50'>
 			<div className='max-w-2xl py-10 m-auto'>
 				<div className='p-6 pb-10 m-4 mb-4 bg-white border rounded-md shadow-md md:p-20 md:m-0'>
-					<FaExclamationTriangle className='m-auto text-8xl text-amber-500' />
+					<FaExclamationCircle className='m-auto text-8xl text-amber-500' />
 					<div className='text-center'>
 						<h1 className='mt-4 mb-2 text-3xl font-bold'>
-							Page Not Found
+							Something Went Wrong
 						</h1>
 						<p className='mb-10 text-xl text-gray-500'>
-							The page you are looking for does not exist.
+							{error.toString()}
 						</p>
 						<Link
 							href='/'
