14af9ab0e01796770b41f45864a508263a0b49c8
jackal7819 <jackal3166@gmail.com>
2024-08-26 19:17:26 +0300

    Add bookmarkProperty action

---
+++ app/actions/bookmarkProperty.ts
@@ -0,0 +1,59 @@
+'use server';
+
+import User from '@/models/User';
+import connectDB from '@/config/database';
+import { Schema, Types } from 'mongoose';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache';
+
+interface BookmarkResult {
+	message: string;
+	isBookmarked: boolean;
+}
+
+export default async function bookmarkProperty(
+	propertyId: string
+): Promise<BookmarkResult> {
+	await connectDB();
+
+	const sessionUser = await getSessionUser();
+
+	if (!sessionUser || !sessionUser.userId) {
+		throw new Error('User ID is required');
+	}
+
+	const { userId } = sessionUser;
+
+	const user = await User.findById(userId);
+
+	if (!user) {
+		throw new Error('User not found');
+	}
+
+	const propertyObjectId = new Schema.Types.ObjectId(propertyId);
+
+	// Type assertion to ensure TypeScript recognizes it as a Mongoose array
+	const bookmarks = user.bookmarks as Types.Array<Schema.Types.ObjectId>;
+
+	let isBookmarked = bookmarks.includes(propertyObjectId);
+
+	let message: string;
+
+	if (isBookmarked) {
+		// If the property is already bookmarked, remove it
+		bookmarks.pull(propertyObjectId);
+		message = 'Bookmark Removed';
+		isBookmarked = false;
+	} else {
+		// If the property is not bookmarked, add it
+		bookmarks.push(propertyObjectId);
+		message = 'Bookmark Added';
+		isBookmarked = true;
+	}
+
+	await user.save();
+
+	revalidatePath('/properties/saved', 'page');
+
+	return { message, isBookmarked };
+}
